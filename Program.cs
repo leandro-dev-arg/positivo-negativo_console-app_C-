cd3658b00cd5341438914cc65e0fa84ee67c4170
leandro <leandro.dev.arg@gmail.com>
2023-12-11 16:53:09 -0300

    primera subida

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace U3_programa2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { int n1;
+           Console.WriteLine("ingrese un numero por favor");
+           n1=int.Parse(Console.ReadLine());
+
+           if (n1>0){
+            Console.WriteLine("el número es positivo");
+           }else{
+            Console.WriteLine("el numero es negativo");
+           }
+        }
+
+        
+    }
+}
